fcbab84a1e70d3a004c88970d8ef37c9a6206e08
nickklosterman <nick_klosterman@yahoo.com>
2014-12-26 21:53:30 -0500

    comment explaining each step in tumblrDLStats.sh

--- tumblr.jsTest/tumblrDLStats.sh
+++ tumblr.jsTest/tumblrDLStats.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 echo "this script outputs a list of the tumblrs whose images have been downloadeed and their respective counts"
-ls | sed 's/_.*//' | sort | uniq -c
+ls | sed 's/_.*//' | sort | uniq -c | sort -h
+#1) get list of files
+#2) strip off the uuid of the file name, retain just the tumblr name
+#3) sort the tumblr names alphabetically
+#4) get a count of the unique names
+#5) sort the counts using the human readable numeric format
