cf14f0d25603267507400f5e54770e98381f8fb2
nickklosterman <nick_klosterman@yahoo.com>
2015-05-04 23:29:41 -0400

    add usage of jpeginfo to eliminate corrupt files

--- tumblr.jsTest/driverScript.sh
+++ tumblr.jsTest/driverScript.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 node ../driver.js 17 ../tumblrListUniq.txt;
 find . -type f -size 0 -print0 | xargs -0 rm #basically RMZ
+
+echo "" > /tmp/jpegCheck.sh
+while read LINE
+do
+    
+    ../../../jpeginfo/jpeginfo -c ${LINE}* | grep -v "[OK]" | sed 's/ .*//;s/^/rm /' >> /tmp/jpegCheck.sh
+    
+done < ../tumblrListUniq.txt
