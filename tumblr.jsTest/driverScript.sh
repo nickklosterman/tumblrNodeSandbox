545c2befd415005cee86c9cbc8e5c20f40762a03
nickklosterman <nick_klosterman@yahoo.com>
2015-05-06 22:43:37 -0400

    added comment to driverscript about jpeginfo usage

--- tumblr.jsTest/driverScript.sh
+++ tumblr.jsTest/driverScript.sh
@@ -5,7 +5,7 @@ find . -type f -size 0 -print0 | xargs -0 rm #basically RMZ
 echo "" > /tmp/jpegCheck.sh
 while read LINE
 do
-    
+#run jpeginfo check on the files, grep the files that aren't shown as 'ok', strip off all the message garbage leaving just the filename and stick a `rm` on the front and pipe it to our file.     
     ../../../jpeginfo/jpeginfo -c ${LINE}* | grep -v "[OK]" | sed 's/ .*//;s/^/rm /' >> /tmp/jpegCheck.sh
     
 done < ../tumblrListUniq.txt
