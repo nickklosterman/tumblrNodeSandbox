a04532dd0a7f122bd107368ffe27896b9ba2fce5
nickklosterman <nick_klosterman@yahoo.com>
2016-03-01 13:06:52 -0500

    refinement of deleteBadImages.sh

--- tumblr.jsTest/deleteBadImages.sh
+++ tumblr.jsTest/deleteBadImages.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #this is meant to be run in the dir where all the images are under the tumblrjstest dir
 cd Images
-find . -name "*.jpg"  -type f -print0 | xargs -0 ../../../jpeginfo/jpeginfo -c | grep -v OK > ../filesToDelete.txt
-sed 's/.jpg .*/.jpg/;s/^/rm /;s/\.\//\.\/Images\//' filesToDelete.txt 
+find . -name "*.jpg"  -type f -print0 | xargs -0 ../../../jpeginfo/jpeginfo -c | grep -v OK > /tmp/filesToDelete.txt
+cd ..
+sed 's/.jpg .*/.jpg/;s/^/rm /;s/\.\//\.\/Images\//' /tmp/filesToDelete.txt > /tmp/deletefiles.sh
+bash /tmp/deletefiles.sh
+
+#now go and do the same for pnga
